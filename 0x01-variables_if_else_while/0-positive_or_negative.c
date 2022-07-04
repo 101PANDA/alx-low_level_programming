efec4950fdf5044179fda8e8b553318e01e62af0
OGIDI FAVOUR <1ogidifavour@gmail.com>
2022-07-03 17:28:39 -0700

    add script to print random number and categorise it

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,20 +4,20 @@
 
 /**
  * main - entry point
- * return 0
+ * Return: 0 (success)
  */
 int main(void)
 {
-  int n;
+int n;
 
-  srand(time(0));
-  n = rand() - RAND_MAX / 2;
-  if (n>0)
-    printf("%d is positive\n");
-    else if (n==0)
-      printf("%d is zero\n");
-    else if (n<0)
-      printf("%d is negative\n");
-  return (0);
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+printf("%d is positive\n");
+else if (n == 0)
+printf("%d is zero\n");
+else if (n < 0)
+printf("%d is negative\n");
+return (0);
 }
 
