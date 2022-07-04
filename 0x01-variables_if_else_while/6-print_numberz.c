b07bd12b7f5734a64001b4c37bfb14659b5f1d10
OGIDI FAVOUR <1ogidifavour@gmail.com>
2022-07-04 12:37:18 -0700

    print all single digit number in base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int x = 48;
+
+	while (x < 58)
+	{
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
