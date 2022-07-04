db0ead09a2aa43ea3dd9f7e9e558d63d84d789f3
OGIDI FAVOUR <1ogidifavour@gmail.com>
2022-07-03 18:22:53 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - outputs a character in the alphabet (lowercase).
+ *
+ * Return: Always 0 if successful.
+ */
+int main(void)
+{
+char i = 'a';
+char j = 'A';
+
+while (i != '{')
+{
+putchar(i);
+i++;
+}
+while (j != '[')
+{
+putchar(j);
+j++;
+}
+putchar('\n');
+return (0);
+}
