966ea3637a8bb1c17b60f75a735d61153bcf2389
FAVOUR OGIDI <1ogidifavour@gmail.com>
2022-07-05 18:38:35 +0000

    *10 alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - entry point
+ * Return: void (success)
+ */
+void print_alphabet_x10(void)
+{
+	char x;
+	int e;
+
+	for (e = 0; e < 10; e++)
+	{
+		for (x = 'a'; x <= 'z'; x++)
+			_putchar(x);
+	}
+	_putchar('\n');
+}
