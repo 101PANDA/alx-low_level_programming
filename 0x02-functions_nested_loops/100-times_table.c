63f34100d36b559a4af687ea197754479e8d0506
FAVOUR OGIDI <1ogidifavour@gmail.com>
2022-07-06 23:44:38 +0000

    not done

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * print_times_table - entry point
+ * @n: times tableto be printed
+ * Return: void (success)
+ */
+void print_times_table(int n)
+{
+	int x, y, z;
+
+	if (!(n > 15) || (n < 0))
+		for (x = 0; x < n; x++)
+		{
+			for (y = 0; y < n; y++)
+			{
+				z = x * y;
+				if (z < 10)
+				{
+					_putchar(' ');
+					_putchar('0' + z);
+				}
+				else if (z < 100)
+				{
+					_putchar('0' + (z / 10));
+					_putchar('0' + (z % 10));
+				}
+				else
+				{
+					_putchar('0' + (z / 100));
+					_putchar('0' + ((z/10) % 10));
+					_putchar('0' + (z % 100));
+				}
+				_putchar(',');
+				_putchar(' ');
+			}
+			_putchar('\n');
+		}
+}
