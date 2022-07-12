8f4a9625bc78e7cd9c07a381ad47bb25f5ee69b9
FAVOUR OGIDI <1ogidifavour@gmail.com>
2022-07-12 22:27:53 +0000

    print string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_rev - prints a string, in reverse, followed by a new line
+ * @s: pointer to the string
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int fwd, bwd = 0;
+
+	for (fwd = 0; s[fwd] != '\0'; fwd++)
+		;
+	for (; bwd <= fwd; --fwd)
+		_putchar(s[fwd]);
+	_putchar('\n');
+}
