9cb5f54b64190fd3c35bab3a544a84846493536e
FAVOUR OGIDI <1ogidifavour@gmail.com>
2022-07-18 17:12:24 +0000

    copies content of a file into another

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * *_memcpy - copies specific bytes
+ * _memcpy - copies specific bytes
  * @src: pointer to the space to be copied
  * @dest: pointer to the space to be filled with the content copied
  * @n: max space to be copied
