2538a43f0633e25ca2fab03a0e65fabd51b0835d
FAVOUR OGIDI <1ogidifavour@gmail.com>
2022-07-18 17:28:06 +0000

    locates char in str

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strchr - locates a character in a string
+ * @s: pointer to the string
+ * @c: character being located
+ * Return: char
+ */
+char *_strchr(char *s, char c)
+{
+	int count;
+
+	for (count = 0; s[count] != c; count++)
+		continue;
+
+	if (s[count] == c)
+		return (&s[count]);
+	else
+		return (0);
+}
