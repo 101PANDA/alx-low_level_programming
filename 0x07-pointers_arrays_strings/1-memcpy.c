f1c4c1545690b3590e485cc68c724a7eb88b16c3
FAVOUR OGIDI <1ogidifavour@gmail.com>
2022-07-18 16:27:22 +0000

    copies content of a file into another

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * *_memcpy - copies specific bytes
+ * @src: pointer to the space to be copied
+ * @dest: pointer to the space to be filled with the content copied
+ * @n: max space to be copied
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int count;
+
+	for (count = 0; count < n; count++)
+	{
+		dest[count] = src[count];
+	}
+	return (dest);
+}
