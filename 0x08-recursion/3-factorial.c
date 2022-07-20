53be0fea1d805d287b21f3e68264afa36264db57
FAVOUR OGIDI <1ogidifavour@gmail.com>
2022-07-20 19:24:31 +0000

    factorials are just recursions with base case

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * factorial - returns factorial of a given number
+ * @n: the given number
+ * REturn: value of th factorial
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (1);
+	return (n * factorial(n -1));
+}
