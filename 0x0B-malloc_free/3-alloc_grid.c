3608e83d2477258ad4e83be098931ae0ec72a599
FAVOUR OGIDI <1ogidifavour@gmail.com>
2022-07-26 23:32:22 +0000

    matrix

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,15 +10,23 @@
 
 int **alloc_grid(int width, int height)
 {
-	int *matrix;
+	int **matrix;
 	int i, j;
 
 	if ((width <= 0) || (height <= 0))
 		return (NULL);
-	matrix = malloc(sizeof(int) * (height * width));
+
+	matrix = malloc(sizeof(int) * height);
 	if (matrix == NULL)
 		return (NULL);
-	for (i = 0; i < (height * width); i++)
-			matrix[i] = 0;
+
+	for (i = 0; i < height; i++)
+		matrix[i] = malloc(sizeof(int) * width);
+	if (matrix[i] == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
+			matrix[i][j] = 0;
 	return (matrix);
 }
