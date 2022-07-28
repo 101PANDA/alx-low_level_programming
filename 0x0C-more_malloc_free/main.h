440979afd30fce6940be13454f933f631edc0353
FAVOUR OGIDI <1ogidifavour@gmail.com>
2022-07-28 20:31:50 +0000

    my own calloc

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -7,4 +7,5 @@ int *array_range(int min, int max);
 char *str_concat(char *s1, char *s2);
 int _strlen(char *s);
 char *_strdup(char *str, int strcount);
+char *_memset(char *s, char b, unsigned int n);
 #endif
