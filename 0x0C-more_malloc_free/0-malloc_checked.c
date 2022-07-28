531921067e555a75fa76136ab33996d507b96024
FAVOUR OGIDI <1ogidifavour@gmail.com>
2022-07-28 17:26:08 +0000

    check malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,6 +1,11 @@
 #include <stdlib.h>
 #include "main.h"
 
+/**
+ * malloc_checked - check if malloc ran
+ * @b: input
+ * Return: pointer to memory allocated by malloc
+ */
 void *malloc_checked(unsigned int b)
 {
 	unsigned int *ptr;
