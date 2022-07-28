6b417bf20f21097d1a148434c220dcd71b882ed5
FAVOUR OGIDI <1ogidifavour@gmail.com>
2022-07-28 17:29:09 +0000

    concatenate 2 strings with refrence to n

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,8 +11,7 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int i;
-	char *ptr, news2;
+	char *ptr, *news2;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -22,7 +21,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (news2 == NULL)
 		return (NULL);
 
-	ptr = 
+	ptr = str_concat(s1, news2);
+	free(news2);
+	if (ptr == NULL)
+		return(NULL);
+
+	return (ptr);
 }
 /**
  * @str: pointer to the string
@@ -47,4 +51,58 @@ char *_strdup(char *str, int strcount)
 	for (i = 0; i < strcount; i++)
 		str2[i] = str[i];
 	return (str2);
+}
+
+/**
+ * str_concat - concates two strings
+ * @s1: first string to concate
+ * @s2: second string to concate
+ * Return: the two strings concated side by side
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int length_s1, length_s2, length_newstr, count, rndno = 0;
+	char *newstr;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	length_s1 = _strlen(s1);
+	length_s2 = _strlen(s2);
+	length_newstr = (length_s1 + length_s2);
+	length_newstr++;
+
+	newstr = malloc(sizeof(char) * length_newstr);
+	if (newstr == NULL)
+		return (NULL);
+
+	for (count = 0; count < length_newstr; count++)
+	{
+		if (count < length_s1)
+			newstr[count] = s1[count];
+		else
+		{
+			newstr[count] = s2[rndno];
+			rndno++;
+		}
+	}
+	return (newstr);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer to the string to be checked
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	return (i);
 }
