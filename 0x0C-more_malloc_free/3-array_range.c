ba2f86756ea8aa0f4e88d241af397e433b6bd436
FAVOUR OGIDI <1ogidifavour@gmail.com>
2022-07-29 17:19:55 +0000

    create array of intiger

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,6 +1,12 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+ * array_range - create an array dependent on the range provided
+ * @min: minimum value of the range
+ * @max: maximum value of the range
+ * Return: array and NULL if min is greater than max or malloc fails
+ */
 int *array_range(int min, int max)
 {
 	int *array, i;
