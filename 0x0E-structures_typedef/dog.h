7f0cce8302a013059dbe14d9f6cfa33123a1ed6a
FAVOUR OGIDI <1ogidifavour@gmail.com>
2022-08-02 00:19:05 +0000

    print a struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
+
 /**
  * struct dog - name, age and owner of dog
  * @name: name of dog
@@ -12,6 +13,11 @@ struct dog
 	float age;
 	char *owner;
 };
+
+/**
+ * dog_t - new name for struct dog
+ */
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
