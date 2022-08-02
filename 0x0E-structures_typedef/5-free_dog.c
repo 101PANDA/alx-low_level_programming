076f7741479f1bf4fdd99d62db5f1037b818ad21
FAVOUR OGIDI <1ogidifavour@gmail.com>
2022-08-02 00:24:30 +0000

    free all space allocated by malloc

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_dog - frees malloc allocated space for struct dog
+ * @d: pointer to the allocated space
+ * Return: void
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
