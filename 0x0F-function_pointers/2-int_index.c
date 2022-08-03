8e22bed7914983248d56c5d53de75e6cbd7f396f
FAVOUR OGIDI <1ogidifavour@gmail.com>
2022-08-03 23:05:48 +0000

    the quest fo int

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,31 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an int in an array based on function passed as cmp
+ * @array: array to search
+ * @size: size of the array
+ * @cmp: function used to search for the set array
+ * Return: index of the wanted int
+ * or -1 if no element matches or size of array is less than or equal to 0
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	if (array && size && cmp)
+	{
+		int x, return_value_of_cmp;
+
+		if (size <= 0)
+			return (-1);
+
+		for (x = 0; x < size; x++)
+		{
+			return_value_of_cmp = cmp(array[x]);
+			if (return_value_of_cmp != 0)
+				return (x);
+		}
+		if (x >= size)
+			return (-1);
+	}
+	return (-1);
+}
