cae780766e23d55de2f690d09dfcfebc9985ac78
FAVOUR OGIDI <1ogidifavour@gmail.com>
2022-08-03 21:47:58 +0000

    pass name to be printed into function pointer

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - passes name into function pointer
+ * @name: name to be passed
+ * @f: function pointer
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+C - Functin pointers
